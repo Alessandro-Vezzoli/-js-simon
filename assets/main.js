9de5538fc0823c6806b4c8a84029ee92a2c6ef9b
alessandro vezzoli <alessandro.vezzoli@vp360.eu>
2024-01-13 13:39:49 +0100

    start working

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+/*Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente 
+deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, 
+il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+//visualizzare 5 numeri casuali (math.random)
+
+//salvarli in una variabile (let)
+
+//far fartire un timer di 30 secondi (setInterval)
+
+//finiti i 30 secondi i numeri scompaiono dallo schermo (addClass= display:none)
+
+//far partire un prompt dove l'utente deve inserire i 5 numeri
+
+//fare un controllo che i 5 numeri inseriti dall'utente siano uguali a quelli casuali nella variabile (let)
+
+//far uscire un alert con scritto se i numeri sono giusti oppure no
